8be1eb9c4e2e693a99656468a14f0713d72a85c6
Linnyck <130088466+Linnyck@users.noreply.github.com>
2023-04-24 14:32:39 -0300

    Create Tarefa04Helper.js

---
+++ Tarefa04Helper.js
@@ -0,0 +1,13 @@
+({
+   
+    showToast : function(component, event, helper) {
+        var toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({
+            "title": "Successo!",
+            "type": "success",
+            "message": "Um Novo produto foi inserido na lista de compras"
+        });
+        toastEvent.fire();
+        $A.get('e.force:refreshView').fire();
+    }
+})
